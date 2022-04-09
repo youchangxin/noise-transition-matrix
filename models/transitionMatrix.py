b5a61451b0f1e6882e087f38327876cab658a4ff
youchangxin <423884489@qq.com>
2022-04-10 01:21:24 +0800

    update

--- models/transitionMatrix.py
+++ models/transitionMatrix.py
@@ -1,10 +1,28 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+import numpy as np
 
 class TransitionMatrix(nn.Module):
     def __init__(self, num_classes, device):
+        super(TransitionMatrix, self).__init__()
+        self.register_parameter(name='w', param=nn.parameter.Parameter(-2 * torch.ones(num_classes, num_classes)))
+
+        self.w.to(device)
+
+        co = torch.ones(num_classes, num_classes)
+        ind = np.diag_indices(co.shape[0])
+        co[ind[0], ind[1]] = torch.zeros(co.shape[0])
+        self.co = co.to(device)
+        self.identity = torch.eye(num_classes).to(device)
+
+    def forward(self):
+        sig = torch.sigmoid(self.w)
+        T = self.identity.detach() + sig * self.co.detach()
+        T = F.normalize(T, p=1, dim=1)
+
+        return T
+        """  
         if num_classes == 10:
             init = -2
         else:
@@ -27,3 +45,4 @@ class TransitionMatrix(nn.Module):
         T = self.identity.detach() + sig * self.coeff.detach()
         T = F.normalize(T, p=1, dim=1)
         return T
+"""
