1413bdbdb516d468177d05c81c2cd22b950e2ddd
youchangxin <423884489@qq.com>
2022-04-10 17:33:27 +0800

    add shell script

--- benchmark.sh
+++ benchmark.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
 
-for dataset in "mnist cifar10 cifar100"
+for dataset in "mnist" "cifar10" "cifar100"
 do
     for noise_rate in 0.2 0.5
     do
-        python3 main.py --dataset $dataset  --noise_type  'symmetric' --noise_rate $noise_rate --device 0
+        python3 train.py --dataset $dataset  --noise_type  'pair' --noise_rate $noise_rate --device 0
     done
 done
 
-for dataset in "mnist cifar10 cifar100"
+for dataset in "mnist" "cifar10" "cifar100"
 do
     for noise_rate in 0.2 0.45
     do
-        python3 main.py --dataset $dataset  --noise_type  'pair' --noise_rate $noise_rate --device 0
+        python3 train.py --dataset $dataset  --noise_type  'pair' --noise_rate $noise_rate --device 0
     done
 done
 
-for dataset in "mnist cifar10 cifar100"
+for dataset in "mnist" "cifar10" "cifar100"
 do
     for noise_rate in 0.2 0.5
     do
-        python3 main.py --dataset $dataset  --noise_type  'symmetric' --noise_rate $noise_rate --device 0 --without_vol
+        python3 train.py --dataset $dataset  --noise_type  'symmetric' --noise_rate $noise_rate --device 0 --without_vol
     done
 done
 
-for dataset in "mnist cifar10 cifar100"
+for dataset in "mnist" "cifar10" "cifar100"
 do
     for noise_rate in 0.2 0.45
     do
-        python3 main.py --dataset $dataset  --noise_type  'pair' --noise_rate $noise_rate --device 0 --without_vol
+        python3 train.py --dataset $dataset  --noise_type  'pair' --noise_rate $noise_rate --device 0 --without_vol
     done
 done
 
