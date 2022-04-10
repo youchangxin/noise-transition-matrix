d209a0694fd2a642bdf75ccf6f016f7ce9012fef
youchangxin <423884489@qq.com>
2022-04-10 17:25:23 +0800

    add shell script

--- benchmark.sh
+++ benchmark.sh
@@ -5,7 +5,7 @@ for dataset in "mnist cifar10 cifar100"
 do
     for noise_rate in 0.2 0.5
     do
-    python3 main.py --dataset $dataset  --noise_type  'symmetric' --noise_rate $noise_rate --device 0
+        python3 main.py --dataset $dataset  --noise_type  'symmetric' --noise_rate $noise_rate --device 0
     done
 done
 
@@ -13,7 +13,7 @@ for dataset in "mnist cifar10 cifar100"
 do
     for noise_rate in 0.2 0.45
     do
-    python3 main.py --dataset $dataset  --noise_type  'pair' --noise_rate $noise_rate --device 0
+        python3 main.py --dataset $dataset  --noise_type  'pair' --noise_rate $noise_rate --device 0
     done
 done
 
@@ -21,7 +21,7 @@ for dataset in "mnist cifar10 cifar100"
 do
     for noise_rate in 0.2 0.5
     do
-    python3 main.py --dataset $dataset  --noise_type  'symmetric' --noise_rate $noise_rate --device 0 --without_vol
+        python3 main.py --dataset $dataset  --noise_type  'symmetric' --noise_rate $noise_rate --device 0 --without_vol
     done
 done
 
@@ -29,7 +29,7 @@ for dataset in "mnist cifar10 cifar100"
 do
     for noise_rate in 0.2 0.45
     do
-    python3 main.py --dataset $dataset  --noise_type  'pair' --noise_rate $noise_rate --device 0 --without_vol
+        python3 main.py --dataset $dataset  --noise_type  'pair' --noise_rate $noise_rate --device 0 --without_vol
     done
 done
 
