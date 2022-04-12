c07a20b33f9f711ea5f51f104b5ad4287007e707
youchangxin <423884489@qq.com>
2022-04-12 10:26:06 +0800

    fix parameter

--- benchmark.sh
+++ benchmark.sh
@@ -5,7 +5,7 @@ for dataset in "mnist" "cifar10" "cifar100"
 do
     for noise_rate in 0.2 0.5
     do
-        python3 train.py --dataset $dataset  --flip_type  'pair' --noise_rate $noise_rate --device 0
+        python3 train.py --dataset $dataset  --flip_type  'symmetric' --noise_rate $noise_rate --device 0
     done
 done
 
